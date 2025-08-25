58f1dc04a2b2c29ac5695efa6cc5c194632a1bfa
OmprakashD20 <omugan07@gmail.com>
2025-08-26 01:07:00 +0530

    [init] setup strata

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to strata!!, version control built in Go")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OmprakashD20/strata
+
+go 1.25.0
